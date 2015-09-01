e5ce83ebf4f2bb27efd62bbb2f1ec533a79891ef
Bobabebelle <bobabebell@gmail.com>
2015-09-01 14:11:52 +0700

    570611034 Sivakorn

---
+++ Lab202/Course.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Lab202
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			/* Testing the Course class */
+			Course cpe200 = new Course("Object-Oriented Programming", "261300");
+			Course isne103 = new Course("Programming Language for ISNE", "269103", "DOME");
+			Course cpe453 = new Course("DIP", "261105", "Dr. Kenneth Cosh", 60);
+
+			Console.WriteLine("\n--- Initial Courses state ---");
+			Console.WriteLine(cpe200);
+			Console.WriteLine(isne103);
+			Console.WriteLine(cpe453);
+
+			/* playing with cpe200 object */
+			Console.WriteLine("\n--- playing with cpe200 object ---");
+			cpe200.setCourseID("261200");
+			Console.WriteLine(cpe200);
+			cpe200.setCourseID("26120O");
+			cpe200.setCourseID("2612002");
+
+			cpe200.setNumStudents(40);
+			Console.WriteLine(cpe200);
+			cpe200.setMaxStudents(60);
+			Console.WriteLine(cpe200);
+			cpe200.setNumStudents(40);
+			Console.WriteLine(cpe200);
+			cpe200.setMaxStudents(35);
+			cpe200.setMaxStudents(40);
+			Console.WriteLine(cpe200);
+
+			/* playing with cpe453 object */
+			Console.WriteLine("\n--- playing with cpe453 object ---");
+			cpe453.setCourseName("Digital Image Processing");
+			cpe453.setCourseID("261453");
+			cpe453.setLecturer("Sansanee");
+			cpe453.setMaxStudents(20);
+			Console.WriteLine(cpe453);
+			Console.ReadLine();
+		}
+	}
+}
